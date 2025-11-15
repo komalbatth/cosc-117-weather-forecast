ee6eb154374c293e29ede2866d172f48fcc8efb4
Komalpreet . <komalpreet.kaur8@myokanagan.bc.ca>
2025-11-14 16:08:03 -0800

    Fix AQI calculation bug

--- backend.py
+++ backend.py
@@ -24,10 +24,13 @@ def process_current(current: ET.Element) -> tuple[str, str, str, ET.Element]:
 
 
 def calc_aqi_level(concentration, intervals) -> str:
+    conc = float(concentration)
+
     for interval_level, interval_max in intervals:
-        if concentration <= str(interval_max):
+        if conc <= float(interval_max):
             return interval_level
-    return 7
+
+    return intervals[-1][0]
 
 
 def air_quality_summary(air_quality: ET.Element) -> str:
